d8e129330c08fb79a3598ff6f5a3ed1601542010
piyushthapa <piyush.codelabs@gmail.com>
2024-12-02 16:05:10 +0545

    safe_append/2 utility function

--- lib/sutra/cardano/utils.ex
+++ lib/sutra/cardano/utils.ex
@@ -17,5 +17,14 @@ defmodule Sutra.Utils do
   def maybe(data, _, f2), do: f2.(data)
 
   def ok_or({:ok, result}, _), do: result
+  def ok_or(_, default) when is_function(default, 0), do: default.()
+  def ok_or(result, default) when is_function(default, 1), do: default.(result)
   def ok_or(_, default), do: default
+
+  def safe_append(list, val) when is_list(list) do
+    new_val = if is_list(val), do: val, else: [val]
+    list ++ new_val
+  end
+
+  def safe_append(_, val), do: if(is_list(val), do: val, else: [val])
 end

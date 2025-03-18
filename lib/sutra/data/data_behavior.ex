1c30b936dc8169e67025a1784cb40a5ae7e05953
piyushthapa <piyush.codelabs@gmail.com>
2025-03-19 03:22:38 +0545

    simple spend Utxo Test

--- lib/sutra/data/data_behavior.ex
+++
@@ -1,7 +0,0 @@
-defmodule Sutra.Cardano.Data.DataBehavior do
-  @moduledoc """
-    Data Behavior
-  """
-  @callback from_plutus(binary(), any()) :: {:ok, any()} | {:error, any()}
-  @callback to_plutus(any()) :: binary()
-end

8d1bf2f6ca79c5b357fc5f86dd87fb2221e21d5d
Aayush Thapa <aayushthapa7@gmail.com>
2025-06-29 15:25:02 +0545

    Refactor provider functions for improved clarity and consistency

--- lib/sutra/provider.ex
+++ lib/sutra/provider.ex
@@ -46,6 +46,30 @@ defmodule Sutra.Provider do
   """
   @callback submit_tx(tx :: Transaction.t() | binary()) :: binary()
 
+  def utxos_at(addresses) do
+    get_provider!().utxos_at(addresses)
+  end
+
+  def utxos_at_refs(refs) do
+    get_provider!().utxos_at_refs(refs)
+  end
+
+  def protocol_params do
+    get_provider!().protocol_params()
+  end
+
+  def slot_config do
+    get_provider!().slot_config()
+  end
+
+  def network do
+    get_provider!().network()
+  end
+
+  def datum_of(datum_hashes) do
+    get_provider!().datum_of(datum_hashes)
+  end
+
   def provider?(mod) do
     case mod.__info__(:attributes)[:behaviour] do
       v when is_list(v) -> Enum.member?(v, __MODULE__)
@@ -53,7 +77,7 @@ defmodule Sutra.Provider do
     end
   end
 
-  def get_provider(provider_type) do
+  def get_provider(provider_type \\ nil) do
     provider =
       case Application.get_env(:sutra, :provider) do
         v when is_list(v) -> v[provider_type]
@@ -61,6 +85,17 @@ defmodule Sutra.Provider do
       end
 
     if provider?(provider), do: {:ok, provider}, else: {:error, "Provider Not found"}
+  end
+
+  @doc """
+    Get provider module, if not found raise error
+  """
+  @spec get_provider!(atom() | nil) :: module()
+  def get_provider!(provider_type \\ nil) do
+    case get_provider(provider_type) do
+      {:ok, mod} -> mod
+      {:error, _} -> raise "Provider Not found"
+    end
   end
 
   def get_fetcher, do: get_provider(:fetch_with)
